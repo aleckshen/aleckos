25db6f0c12b7c43aea5d6ec3cd9623311fabf4b2
aleckshen <aleckshn@gmail.com>
2026-05-04 10:52:04 +1200

    fix(config): use turbopack to load markdown

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      type: "asset/source",
-    });
-    return config;
+  turbopack: {
+    rules: {
+      "*.md": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+    },
   },
 };
 
