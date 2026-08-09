734552fe0f10327b0af16c329aa6c02d5e1f6c68
aleckshen <aleckshn@gmail.com>
2026-08-09 15:26:14 +1200

    feat(content): add longform content module

---
+++ src/content/longform.ts
@@ -0,0 +1,7 @@
+import aboutMd from "./about.md";
+import experienceMd from "./experience.md";
+import projectsMd from "./projects.md";
+
+export const aboutLong = aboutMd;
+export const projectsLong = projectsMd;
+export const experienceLong = experienceMd;
