979ad16395fd19c3193e58850f9b9c8c51e88ba3
aleckshen <aleckshn@gmail.com>
2026-08-13 11:30:05 +1200

    feat(markdown): add heading hierarchy and code syntax highlighting

--- src/components/apps/TextViewer.tsx
+++ src/components/apps/TextViewer.tsx
@@ -1,5 +1,6 @@
 "use client";
 import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
 
 type Props = { content: string };
@@ -7,7 +8,12 @@ type Props = { content: string };
 export function TextViewer({ content }: Props) {
   return (
     <div className="prose prose-invert h-full max-w-none p-6">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeHighlight]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
