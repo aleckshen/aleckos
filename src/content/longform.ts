cc80a9b0543898b360954ec0f4a32748037eb1ad
aleckshen <aleckshn@gmail.com>
2026-08-17 13:33:09 +1200

    refactor(content): move markdown files into markdown/

--- src/content/longform.ts
+++ src/content/longform.ts
@@ -1,6 +1,6 @@
-import aboutMd from "./about.md";
-import experienceMd from "./experience.md";
-import projectsMd from "./projects.md";
+import aboutMd from "./markdown/about.md";
+import experienceMd from "./markdown/experience.md";
+import projectsMd from "./markdown/projects.md";
 
 export const aboutLong = aboutMd;
 export const projectsLong = projectsMd;
